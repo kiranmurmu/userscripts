2f56dc9e522f98dc077eb5ecebc35dfe04d3d03c
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2025-08-13 02:34:08 +0530

    Update Reader View icon

--- reader-view/main.user.js
+++ reader-view/main.user.js
@@ -5,12 +5,13 @@
 // @copyright       2025+, Kiran Murmu
 // @match           *://*/*
 // @author          kiranmurmu
-// @version         0.1.0
+// @version         0.1.1
 // @homepage        https://github.com/kiranmurmu/userscripts
 // @source          https://github.com/kiranmurmu/userscripts/tree/main/reader-view
 // @license         MIT
 // @run-at          document-start
 // @grant           GM_registerMenuCommand
+// @icon            https://icons.duckduckgo.com/ip3/tampermonkey.net.ico
 // @require         https://cdnjs.cloudflare.com/ajax/libs/readability/0.6.0/Readability.min.js
 // ==/UserScript==
 
