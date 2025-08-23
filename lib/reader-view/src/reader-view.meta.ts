0c79f1a75ff4cb4a5c96a54f2a2a0047bfa63a16
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2025-08-23 12:00:53 +0530

    refactor(reader-view): create userscript metadata file

---
+++ lib/reader-view/src/reader-view.meta.ts
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            Reader View (Readability)
+// @namespace       https://kiranmurmu.github.io/
+// @description     Reader View (Mozilla Readability) for Tampermonkey
+// @copyright       2025+, Kiran Murmu
+// @match           *://*/*
+// @author          kiranmurmu
+// @version         0.1.1
+// @homepage        https://github.com/kiranmurmu/userscripts
+// @source          https://github.com/kiranmurmu/userscripts/tree/main/reader-view
+// @license         MIT
+// @run-at          document-start
+// @grant           GM_registerMenuCommand
+// @icon            https://icons.duckduckgo.com/ip3/tampermonkey.net.ico
+// @require         https://cdnjs.cloudflare.com/ajax/libs/readability/0.6.0/Readability.min.js
+// ==/UserScript==
+
