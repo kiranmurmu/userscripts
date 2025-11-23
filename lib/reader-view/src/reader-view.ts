e779d494be0237f25fd34ba52ae359ebbc2ceb4d
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2025-11-23 12:00:30 +0530

    chore(reader-view): clean unused codes

--- lib/reader-view/src/reader-view.ts
+++ lib/reader-view/src/reader-view.ts
@@ -1,22 +1,3 @@
-// ==UserScript==
-// @name                    Reader View (Readability)
-// @namespace               https://github.com/kiranmurmu/userscripts
-// @description             Reader View (Mozilla Readability) for Tampermonkey
-// @copyright               2025+, Kiran Murmu
-// @match                   *://*/*
-// @author                  kiranmurmu
-// @version                 0.1.1
-// @homepage                https://github.com/kiranmurmu/userscripts
-// @source                  https://github.com/kiranmurmu/userscripts/tree/main/reader-view
-// @license                 MIT
-// @run-at                  document-start
-// @grant                   GM_registerMenuCommand
-// @grant                   GM_getResourceURL
-// @icon                    https://www.tampermonkey.net/images/icon.png
-// @require                 https://cdnjs.cloudflare.com/ajax/libs/readability/0.6.0/Readability.min.js
-// @resource reader.css     https://github.com/kiranmurmu/userscripts/raw/refs/heads/main/lib/reader-view/style/reader.css
-// ==/UserScript==
-
 import type MozillaReadability from "mozilla-readability";
 
 type Readability = MozillaReadability;
